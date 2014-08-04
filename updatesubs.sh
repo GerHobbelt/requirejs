cb28a727fa369fb6ded8202c69a57a4fa2d7436f
Tom Green <tomg@cpanel.net>
2014-08-04 12:37:14 -0500

    Tweaked the update script to be fault tollerant

--- updatesubs.sh
+++ updatesubs.sh
@@ -10,62 +10,98 @@ cd ../r.js
 node dist.js
 cd ../requirejs
 
-# The RequireJS+jQuery sample project.
-echo "Updating jQuery sample project"
-cp require.js ../require-jquery/parts/require.js
-cp ../r.js/r.js ../require-jquery/jquery-require-sample/r.js
-cd ../require-jquery/parts
-./update.sh
-cd ../../requirejs
+if [ -e ../require-jquery/ ]
+    then
+    # The RequireJS+jQuery sample project.
+    echo "Updating jQuery sample project"
+    cp require.js ../require-jquery/parts/require.js
+    cp ../r.js/r.js ../require-jquery/jquery-require-sample/r.js
+    cd ../require-jquery/parts
+    ./update.sh
+    cd ../../requirejs
+fi
 
 # The sample projects
 
-echo "Updating requirejs/example-jquery-cdn"
-cp require.js ../example-jquery-cdn/www/js/lib/require.js
-cp ../r.js/r.js ../example-jquery-cdn/tools/r.js
+if [ -e ../example-jquery-cdn/ ]
+    then
+    echo "Updating requirejs/example-jquery-cdn"
+    cp require.js ../example-jquery-cdn/www/js/lib/require.js
+    cp ../r.js/r.js ../example-jquery-cdn/tools/r.js
+fi
 
-echo "Updating requirejs/example-jquery-shim"
-cp require.js ../example-jquery-shim/www/js/lib/require.js
-cp ../r.js/r.js ../example-jquery-shim/tools/r.js
+if [ -e ../example-jquery-shim/ ]
+    then
+    echo "Updating requirejs/example-jquery-shim"
+    cp require.js ../example-jquery-shim/www/js/lib/require.js
+    cp ../r.js/r.js ../example-jquery-shim/tools/r.js
+fi
 
-echo "Updating requirejs/example-multipage"
-cp require.js ../example-multipage/www/js/lib/require.js
-cp ../r.js/r.js ../example-multipage/tools/r.js
+if [ -e ../example-multipage/ ]
+    then
+    echo "Updating requirejs/example-multipage"
+    cp require.js ../example-multipage/www/js/lib/require.js
+    cp ../r.js/r.js ../example-multipage/tools/r.js
+fi
 
-echo "Updating requirejs/example-multipage-shim"
-cp require.js ../example-multipage-shim/www/js/lib/require.js
-cp ../r.js/r.js ../example-multipage-shim/tools/r.js
+if [ -e ../example-multipage-shim/ ]
+    then
+    echo "Updating requirejs/example-multipage-shim"
+    cp require.js ../example-multipage-shim/www/js/lib/require.js
+    cp ../r.js/r.js ../example-multipage-shim/tools/r.js
+fi
 
-echo "Updating requirejs/example-libglobal"
-cp require.js ../example-libglobal/lib/require.js
-cp ../r.js/r.js ../example-libglobal/tools/r.js
+if [ -e ../example-libglobal/ ]
+    then
+    echo "Updating requirejs/example-libglobal"
+    cp require.js ../example-libglobal/lib/require.js
+    cp ../r.js/r.js ../example-libglobal/tools/r.js
+fi
 
-echo "Updating volojs/create-template"
-cp require.js ../../volojs/create-template/www/lib/require.js
-cp ../r.js/r.js ../../volojs/create-template/tools/r.js
+if [ -e ../../volojs/create-template/ ]
+    then
+    echo "Updating volojs/create-template"
+    cp require.js ../../volojs/create-template/www/lib/require.js
+    cp ../r.js/r.js ../../volojs/create-template/tools/r.js
+fi
 
-echo "Updating volojs/create-responsive-template"
-cp require.js ../../volojs/create-responsive-template/www/js/lib/require.js
-cp ../r.js/r.js ../../volojs/create-responsive-template/tools/r.js
+if [ -e ../../volojs/create-responsive-template/ ]
+    then
+    echo "Updating volojs/create-responsive-template"
+    cp require.js ../../volojs/create-responsive-template/www/js/lib/require.js
+    cp ../r.js/r.js ../../volojs/create-responsive-template/tools/r.js
+fi
 
-# The cajon project
-echo "Updating the cajon project"
-cp require.js ../cajon/tools/require.js
-cp ../r.js/r.js ../cajon/tools/r.js
-cd ../cajon/tools
-./build-cajon.js
-cd ../../requirejs
+if [ -e ../cajon/tools/ ]
+    then
+    # The cajon project
+    echo "Updating the cajon project"
+    cp require.js ../cajon/tools/require.js
+    cp ../r.js/r.js ../cajon/tools/r.js
+    cd ../cajon/tools
+    ./build-cajon.js
+    cd ../../requirejs
+fi
 
-# The require-cs project
-echo "Updating the require-cs CoffeeScript plugin"
-cp require.js ../require-cs/demo/lib/require.js
-cp ../r.js/r.js ../require-cs/tools/r.js
+if [ -e ../require-cs/ ]
+    then
+    # The require-cs project
+    echo "Updating the require-cs CoffeeScript plugin"
+    cp require.js ../require-cs/demo/lib/require.js
+    cp ../r.js/r.js ../require-cs/tools/r.js
+fi
 
-# The npm container stuff
-echo "Updating requirejs-npm"
-cp require.js ../requirejs-npm/requirejs/require.js
-cp ../r.js/r.js ../requirejs-npm/requirejs/bin/r.js
+if [ -e ../requirejs-npm/ ]
+    then
+    # The npm container stuff
+    echo "Updating requirejs-npm"
+    cp require.js ../requirejs-npm/requirejs/require.js
+    cp ../r.js/r.js ../requirejs-npm/requirejs/bin/r.js
+fi
 
-# The bower container stuff
-echo "Updating requirejs-bower"
-cp require.js ../requirejs-bower/require.js
+if [ -e ../requirejs-bower/ ]
+    then
+    # The bower container stuff
+    echo "Updating requirejs-bower"
+    cp require.js ../requirejs-bower/require.js
+fi
